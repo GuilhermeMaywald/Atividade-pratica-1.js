dc3cd87b9234fb7de9921dc7226d37f05f00beb7
guilherme maywald <guilhermemaywald@gmail.com>
2022-08-25 08:00:38 -0300

    Atividade Pratica 1 JS

---
+++ js/atividade-1.js
@@ -0,0 +1,10 @@
+// Monte um script em um arquivo com nome de aula1_js.html, que calcule a soma a de 10 + 5 e devolva seu resultado através do console em formato numérico. 
+
+let a = 0;
+let b = 0;
+let resultado = 0;
+
+a = 10;
+b = 5;
+resultado = a + b;
+console.log(parseInt(a) + parseInt(b));
---
+++ js/atividade-2.js
@@ -0,0 +1,10 @@
+// Receba um valor através do comando prompt no mesmo arquivo onde criou o seu primeiro cálculo, e exiba este valor no console do Google Chrome.
+
+let a = 0;
+let b = 0;
+let resultado = 0;
+
+a = prompt('Digite o primeiro número: ');
+b = prompt('Digite o segundo número: ');
+resultado = a + b;
+console.log(parseInt(a) + parseInt(b));
